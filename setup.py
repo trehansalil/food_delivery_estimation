48a84907786c2b2950ca931af85308b8cb460104
Salil Trehan <trehansalil1@gmail.com>
2024-04-04 03:11:28 +0000

    Other files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='hate-speech-classification',
+    name='food-estimation-problem',
     version='0.0.1',
     author='Salil Trehan',
     author_email='trehansalil1@gmail.com',
